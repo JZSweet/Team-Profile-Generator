286d2ee1f82cd74f6e38badf4ecb529f41a2b2b0
JZSweet <sweet_zhaozhao@live.cn>
2021-01-28 15:57:22 -0800

    something

---
+++ HTML-generator.js
@@ -0,0 +1,6 @@
+console.log("hi im HTML generator")
+function run(name){
+    return `<h1>hi ${name}</h1>`
+}
+
+module.exports = run
---
+++ index.js
@@ -0,0 +1,5 @@
+const output = require("./HTML-generator")
+
+console.log("hallo world",output("jz"))
+
+
